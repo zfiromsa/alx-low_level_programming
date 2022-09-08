a77c5dc64baffc32fbe3371706ec89b9a9f38b32
zfiromsa <mdzm842@gmail.com>
2022-09-08 11:14:25 -0700

    go

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,10 @@
 #include <stdio.h>
 
-
-/* main print out using puts*/
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
