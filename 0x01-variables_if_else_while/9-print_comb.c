eb8537a2219e1931c5e5839a7a18aa4fe2380918
zfiromsa <mdzm842@gmail.com>
2022-09-09 18:34:43 -0700

    task8

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+    int ch ;
+    
+    for (ch = 48; ch <= 57; ch++)
+    {
+        putchar(ch);
+        putchar(44);
+        putchar(32);
+    }
+    putchar('\n');
+    return (0);
+}
