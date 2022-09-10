3c21fe75d41ead23576da35e826f1795d5a0e761
zfiromsa <mdzm842@gmail.com>
2022-09-10 13:50:00 -0700

    0

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -15,8 +15,9 @@
 
 	printf("Size of a char: %d byte(s)\n", sizeof(c));
 	printf("Size of an int: %d byte(s)\n", sizeof(i));
-        printf("Size of a long int: %d byte(s)\n",sizeof(l));
+    printf("Size of a long int: %d byte(s)\n",sizeof(l));
 	printf("size of a long long int: %d byte(s)\n",sizeof(ll));
 	printf("Size of a float: %d byte(s)\n",sizeof(f));
+	fprintf( stderr, "my %s has %d chars\n", "string format", 30);
 	return (0);
 }
