80272e1143b9d7c9df79c670853561e4c6a640c7
zfiromsa <mdzm842@gmail.com>
2022-09-09 17:56:49 -0700

    task3

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void) 
+{
+   char ch;
+
+   for(ch = 'a' ; ch <= 'z' ; ch++)
+   {
+      if (ch != 65 && ch != 71 )
+      {
+        putchar(ch);
+      }
+      
+   }
+   putchar('\n');
+   return(0);
+}
