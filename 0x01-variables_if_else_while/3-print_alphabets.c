80876d795a9add875c58085c6f6c28ac45cf2bcd
zfiromsa <mdzm842@gmail.com>
2022-09-09 17:51:05 -0700

    task3

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -9,11 +9,11 @@ int main(void)
 {
    char ch;
 
-   for(ch = 65 ; ch <= 90 ; ch++)
+   for(ch = 97 ; ch <= 122 ; ch++)
    {
       putchar(ch);
    }
-   for(ch = 97 ; ch <= 122 ; ch++)
+   for(ch = 65 ; ch <= 90 ; ch++)
    {
       putchar(ch);
    }
