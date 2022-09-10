a57eea802f7e455ed29b4bc620d9aa641b04bfcd
zfiromsa <mdzm842@gmail.com>
2022-09-09 18:38:01 -0700

    task8

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -12,8 +12,11 @@ int main(void)
     for (ch = 48; ch <= 57; ch++)
     {
         putchar(ch);
-        putchar(44);
-        putchar(32);
+        if(ch <=56)
+        {
+            putchar(44);
+            putchar(32);
+        }
     }
     putchar('\n');
     return (0);
