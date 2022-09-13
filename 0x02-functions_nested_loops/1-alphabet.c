f03096aee980af316c5a126f1f9192866ef048fd
zfiromsa <mdzm842@gmail.com>
2022-09-13 15:41:22 -0700

    0-putchar.c

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/*
+ * print_alphabet: - print out lower case alphabet.
+
+ * Return: Always 0.
+ */
+void print_alphabet(void)
+{
+char ch;
+
+for (ch = 'a'; ch <= 'z'; ch++)
+{
+_putchar(ch);
+}
+_putchar('\n');
+}
