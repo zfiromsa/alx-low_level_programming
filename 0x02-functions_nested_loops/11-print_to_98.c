c00ac802dce81c9c58c9135b137a905d5a4e1526
zfiromsa <mdzm842@gmail.com>
2022-09-14 02:56:24 -0700

    5

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -3,11 +3,26 @@
 void print_to_98(int n)
 {
 int i;
-
-for (i = n; i >= 98; i++ )
+if (i <= 98)
+{
+for (i = n; i <= 98; i++ )
 {
 _putchar(i);
 _putchar(',');
 _putchar(' ');
+}
+}
+else if (i >= 98)
+{
+for (i = n; i >= 98; i-- )
+{
+_putchar(i);
+_putchar(',');
+_putchar(' ');
+}
+else
+{
+_putchar(i);
+}
 }
 }
