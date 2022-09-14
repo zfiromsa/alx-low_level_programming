bcfe9d313bcdf774f159763cdd0b864390e8976e
zfiromsa <mdzm842@gmail.com>
2022-09-13 19:30:27 -0700

    5

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,6 @@
+#include "main.h"
+
+int add(int i, int j)
+{
+return (i+j);
+}
--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -5,8 +5,10 @@ void print_to_98(int n)
 int i;
 
 for (i = n; i >= 98; i++ )
+{
 _putch(i);
-_putch(',')
-_putch(' ')
+_putch(',');
+_putch(' ');
+}
 return (0);
 }
