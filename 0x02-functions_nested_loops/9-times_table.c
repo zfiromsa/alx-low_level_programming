aec11de55add5c3e0311a3d3117a5a7941e2e50c
zfiromsa <mdzm842@gmail.com>
2022-09-14 05:27:33 -0700

    9

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -3,11 +3,11 @@
 void times_table(void)
 {
 int i,j;
-for (i=0; i <= 9; i++)
+for (i='0'; i <= '9'; i++)
 {
-for (j=0; j <= 9; j++)
+for (j='0'; j <= '9'; j++)
 {
-if (i!=9)
+if (i!= '9')
 {
 _putchar(j*i);
 _putchar(',');
