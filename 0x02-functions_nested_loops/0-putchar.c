7e823e582491d9658c0d7c71bf79d1f53df83c98
zfiromsa <mdzm842@gmail.com>
2022-09-13 19:54:11 -0700

    5

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -6,14 +6,13 @@
  
 int _putchar()
 {
-_putchar('_');
-_putchar('p');
-_putchar('u');
-_putchar('t');
-_putchar('c');
-_putchar('h');
-_putchar('a');
-_putchar('r');
-_putchar('\n');
+char ch[]={'_','p','u','t','c','h','a','r','\n'};
+int i=0;
+
+while (i<9)
+{
+_putchar(ch[i]);
+i++;
+}
 return (0);
 }
