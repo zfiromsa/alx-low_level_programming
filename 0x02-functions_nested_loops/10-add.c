c67a5124aec1be078833b2c9c4365de3d255eec7
zfiromsa <mdzm842@gmail.com>
2022-09-14 13:30:36 -0700

    9

--- 0x02-functions_nested_loops/10-add.c
+++ 0x02-functions_nested_loops/10-add.c
@@ -1,4 +1,12 @@
 #include "main.h"
+/**
+ * function_name - Short description, single line
+ * @i_and_j: Description of parameter x
+(* a blank line
+ * Description: Longer description of the function)?
+(* section header: Section description)*
+ * Return: Description of the returned value
+ */
 
 int add(int i, int j)
 {
