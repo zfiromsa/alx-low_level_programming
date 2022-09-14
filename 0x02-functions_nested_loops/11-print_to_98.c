b0b3258630ede92f68689e075d61c4702d1d8718
zfiromsa <mdzm842@gmail.com>
2022-09-14 12:18:16 -0700

    9

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -7,19 +7,21 @@ if (n > 98)
 {
 while (n > 98)
 {
-printf("%d, ",n--);
-printf("%d",n);
+printf("%d, " , n--);
+printf("%d" , n);
 }
 }
 else if (n == 98)
 {
-printf("%d",n);
+printf("%d" , n);
 }
 else
 {
 while (n < 98)
-printf ("%d, ",n++);
-printf("%d",n);
+{
+printf ("%d, " , n++);
+printf("%d" , n);
 }
 printf("\n");
+}
 }
