e3050770dce992c44afd617cf2ecb95070032859
zfiromsa <mdzm842@gmail.com>
2022-09-13 19:25:36 -0700

    5

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+void print_to_98(int n)
+{
+int i;
+
+for (i = n; i >= 98; i++ )
+_putch(i);
+_putch(',')
+_putch(' ')
+return (0);
+}
--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -5,15 +5,6 @@ int print_last_digit(int n)
 int lastDigit;
 
 lastDigit = n % 10;
-if (lastDigit < 0)
-{
-lastDigit = -lastDigit;
-}
-else
-{
-lastDigit = lastDigit;
-}
-_putchar(lastDigit);
 _putchar(lastDigit);
 return (0);
 }
