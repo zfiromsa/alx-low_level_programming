49f557d4aebd3a6f03a03f920e4d9ff47473e065
zfiromsa <mdzm842@gmail.com>
2022-09-14 12:36:43 -0700

    9

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -12,13 +12,9 @@ int print_last_digit(int n)
 int lastDigit;
 
 lastDigit = n % 10;
-if ( n < 0)
+if ( lastDigit < 0)
 {
-lastDigit = lastDigit * (-1);
-_putchar(lastDigit);
-return (lastDigit);
-}
-else
-_putchar(lastDigit);
+lastDigit *= -1;
+_putchar(lastDigit + '0');
 return (lastDigit);
 }
