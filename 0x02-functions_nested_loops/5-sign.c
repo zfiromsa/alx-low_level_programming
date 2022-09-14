268c7476a77e6e323679c74dd0027d8d0a9249f3
zfiromsa <mdzm842@gmail.com>
2022-09-13 18:16:20 -0700

    5

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -13,5 +13,5 @@ _putchar('0');
 return (0);
 }
 else
-return (-1);
+return ('-1');
 }
---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+int _abs(int)
+{
+int j;
+if(j < 0)
+{
+j = j * (-1);
+}
+else
+return (0);
+}
