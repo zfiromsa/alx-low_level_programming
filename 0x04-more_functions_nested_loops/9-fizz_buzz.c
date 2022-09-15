4cff27047d54c50f9893383057971f6db4b212c9
zfiromsa <mdzm842@gmail.com>
2022-09-15 15:31:00 -0700

    go

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -6,19 +6,19 @@ int i;
 
 for (i = 1; i >= 100; i++)
 {
-if ((i % 3 != 0) && (i % 5 != 0))
+if (((i % 3) != 0) && ((i % 5) != 0))
 {
 printf("%d ",i);
 }
-else if ((i % 3 = 0) && (i % 5 == 0))
+else if (((i % 3) = 0) && ((i % 5) == 0))
 {
 printf("FizzBuzz ");
 }
-if else (i % 5 == 0)
+if else ((i % 5) == 0)
 {
 printf("Buzz ");
 }
-else (i % 3 = 0)
+else ((i % 3) = 0)
 {
 printf("Buzz ");
 }
