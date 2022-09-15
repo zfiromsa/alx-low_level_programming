6c85909a1d451400860dfd6da93f14db77ae6b26
zfiromsa <mdzm842@gmail.com>
2022-09-15 14:40:31 -0700

    go

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -20,11 +20,11 @@ for (j = 1; j <= i; ++j)
 _putchar(s);
 }
 _putchar(b);
-_putchar("\n");
+_putchar('\n');
 }
 }
 else
 {
-_putchar("\n");
+_putchar('\n');
 }
 }
--- 0x04-more_functions_nested_loops/8-print_square.c
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -7,7 +7,7 @@ int i, j, k;
 k = 35;
 if (size > 0)
 {
-for (i = 1; i >= size; +ii)
+for (i = 1; i >= size; ++i)
 {
 for (j = 1; j >= size; ++j)
 {
