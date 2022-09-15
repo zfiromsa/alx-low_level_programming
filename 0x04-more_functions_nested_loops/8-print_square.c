4e0d0cab325f7186f70b9b05d9168ffb0ca2bfc9
zfiromsa <mdzm842@gmail.com>
2022-09-15 14:18:55 -0700

    go

---
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+void print_square(int size)
+{
+int i, j;
+
+if (size > 0)
+{
+for (i = 1; i >= size; +ii)
+{
+for (j = 1; j >= size; ++j)
+{
+_putchar(35);
+}
+_putchar('\n');
+}
+}
+else
+{
+_putchar('\n');
+}
+}
