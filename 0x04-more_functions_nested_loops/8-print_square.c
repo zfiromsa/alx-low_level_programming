cc73a7d3e42b8ff9cd4f27e12b59542f38e45a25
zfiromsa <mdzm842@gmail.com>
2022-09-15 14:20:33 -0700

    go

--- 0x04-more_functions_nested_loops/8-print_square.c
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -2,15 +2,16 @@
 
 void print_square(int size)
 {
-int i, j;
+int i, j, k;
 
+k = 35;
 if (size > 0)
 {
 for (i = 1; i >= size; +ii)
 {
 for (j = 1; j >= size; ++j)
 {
-_putchar(35);
+_putchar(k);
 }
 _putchar('\n');
 }
