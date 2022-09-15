138d3d1016b69f8795dee51733cd06cc13150bee
zfiromsa <mdzm842@gmail.com>
2022-09-15 13:03:07 -0700

    go

---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+void print_diagonal(int n)
+{
+int i, j;
+if (n < 0)
+{
+for (i = 1; i <= n; ++i)
+{
+for (j = 1; j <= i; ++j)
+{
+_putchar(s);
+}
+}
+_putchar("\n");
+}
+else
+{
+_putchar("\n");
+}
+}
