a54d14be019c183d67f78fba9293cb697361df4f
zfiromsa <mdzm842@gmail.com>
2022-09-15 02:41:14 -0700

    main.h

--- 0x03-debugging/main.h
+++ 0x03-debugging/main.h
@@ -1,3 +1,10 @@
+#define MAIN_H
+#include <stdio.h>
+
 positive_or_negative();
 int largest_number(int a, int b, int c);
 void print_remaining_days(int, int, int);
+int convert_day(int month, int day);
+
+
+#endif /*MAIN_H/
