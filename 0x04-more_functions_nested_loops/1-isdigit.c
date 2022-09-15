44dbf36a300f6738247ea995ad305f3177a463d3
zfiromsa <mdzm842@gmail.com>
2022-09-15 07:16:17 -0700

    go

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/*
+ * _islower: - it is func checks for alphabetic character.
+ * @c: it is an argumint.
+ * Return: 1 0r 0.
+ */
+
+int _isdigit(int c)
+{
+if (c >= 48 && c <= 57)
+{
+return (1);
+}
+else
+return (0);
+}
