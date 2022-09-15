40f11d173932bac4e05d31b70657a23c72d2e895
zfiromsa <mdzm842@gmail.com>
2022-09-15 12:43:38 -0700

    go

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -5,14 +5,20 @@ void print_line(int n)
 int j, i, in;
 
 j = 1;
-i =95;
+i = 95;
 in = n;
-if (in > 0)
+if (in >= 0)
+{
+if (in != 0)
 {
 while (j >= in)
 {
 _putchar(i);
 j++;
+}
+}
+else
+{
 }
 }
 }
