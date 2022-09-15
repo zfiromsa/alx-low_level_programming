5056973b3e9b5ffd30c3166fab3b8d5f26e01380
zfiromsa <mdzm842@gmail.com>
2022-09-15 10:57:23 -0700

    go

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+void more_numbers(void)
+{
+int i, j;
+
+for (i = 48; i <= 58; i++)
+{
+if(i != 58)
+{
+_putchar(i);
+}
+else
+{
+for(j = 49; j < 49; j++)
+{
+_putchar(j);
+for(j = 48; j <=52; j++)
+{
+_putchar(j);
+}
+}
+}
+}
+_putchar('\n');
+}
