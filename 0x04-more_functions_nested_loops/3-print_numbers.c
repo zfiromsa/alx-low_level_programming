3b1685a9a3d76a08c5cb3f713cc8efdea3caf207
zfiromsa <mdzm842@gmail.com>
2022-09-15 09:39:35 -0700

    go

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -3,7 +3,8 @@
 void print_numbers(void)
 {
 int i;
-for (i = 48; i >= 57; i++)
+
+for (i = 48; i <= 57; i++)
 {
 _putchar(i);
 }
--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -1,11 +1,12 @@
 #include "main.h"
 
-void print_numbers(void)
+void print_most_numbers(void)
 {
 int i;
+
 for (i = 48; i >= 57; i++)
 {
-if(i != 50 i != 52)
+if((i != 50) && (i != 52))
 {
 _putchar(i);
 }
