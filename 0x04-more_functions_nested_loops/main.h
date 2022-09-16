47581597493946e7203391a25f06d8d568790358
zfiromsa <mdzm842@gmail.com>
2022-09-16 04:43:23 -0700

    go

--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -1,8 +1,6 @@
 #ifndef MAIN_H
 #define MAIN_H
 
-int _putchar(char c);
-int _putchar(int c);
 int _isupper(int c);
 int _isdigit(int c);
 int mul(int a, int b);
@@ -14,5 +12,6 @@ void print_diagonal(int n);
 void print_square(int size);
 void print_triangle(int size);
 void fizz_buzz(void);
+int _putchar(char c);
 
 #endif  
