6f622663cd438743d8212e9329a47c430be26927
zfiromsa <mdzm842@gmail.com>
2022-09-19 13:34:19 -0700

    task 4

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * print_rev - prints a string revirs.
+ * @s: pointer to an char as parameter
+ * Return: return void
+ */ 
+
+void print_rev(char *s)
+{
+int j;
+
+int i = 0;
+while (*(str + i) != '\0')
+{
+len = i;
+i++;
+}
+while (len--)
+{
+putchar(*(s + len));
+}
+putchar(10);
+}
