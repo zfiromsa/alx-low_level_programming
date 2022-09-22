98574449d20f2ff71888fbce44602ef0c02f3871
zfiromsa <mdzm842@gmail.com>
2022-09-22 13:05:49 -0700

    0x06-pointers

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -11,23 +11,23 @@ len = strlen(s);
 char *str = malloc(len - 1);
 while (i < len)
 {
-if (s[i] == ('a' || 'A')
+if (s[i] == ('a' || 'A'))
 {
 str[i] = '4';
 }
-else if (s[i] == ('e' || 'E')
+else if (s[i] == ('e' || 'E'))
 {
 str[i] = '3';
 }
-else if (s[i] == ('o' || 'O')
+else if (s[i] == ('o' || 'O'))
 {
 str[i] = '0';
 }
-else if (s[i] == ('t' || 'T')
+else if (s[i] == ('t' || 'T'))
 {
 str[i] = '7';
 }
-else if (s[i] == ('l' || 'L')
+else if (s[i] == ('l' || 'L'))
 {
 str[i] = '1';
 }
