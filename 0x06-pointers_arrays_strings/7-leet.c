0d74f9a991cb529e038bcc433d20728f41d35f5e
zfiromsa <mdzm842@gmail.com>
2022-09-22 12:51:40 -0700

    0x06-pointers

---
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -0,0 +1,38 @@
+#include "main.h"
+#include <string>
+
+char *leet(char *s)
+{
+int i, len;
+
+i = 0;
+len = strlen(s);
+while (i < len)
+{
+if (s[i] = ('a' || 'A')
+{
+s[i] = '4';
+}
+else if (s[i] = ('e' || 'E')
+{
+s[i] = '3';
+}
+else if (s[i] = ('o' || 'O')
+{
+s[i] = '0';
+}
+else if (s[i] = ('t' || 'T')
+{
+s[i] = '7';
+}
+else if (s[i] = ('l' || 'L')
+{
+s[i] = '1';
+}
+else
+{
+}
+i++;
+}
+return (s);
+}
