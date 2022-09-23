07ff799316bbb7f695a44f6ffa74338263d6eadf
zfiromsa <mdzm842@gmail.com>
2022-09-23 02:43:28 -0700

    0x06-pointers

--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -1,10 +1,12 @@
 #ifndef MAIN_H
 #define MAIN_H
 
-int _strlen(char *s);
 char *_strcat(char *dest, char *src);
 char *_strncat(char *dest, char *src, int n);
+char *_strncpy(char *dest, char *src, int n);
 void reverse_array(int *a, int n);
-char *leet(char *s);
+char *string_toupper(char *);
+char *leet(char *);
+char *cap_string(char *);
 
 #endif
