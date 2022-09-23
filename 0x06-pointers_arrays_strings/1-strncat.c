a06aec83378d9b509136a1499ff5633003540a4c
zfiromsa <mdzm842@gmail.com>
2022-09-23 02:29:22 -0700

    0x06-pointers

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -1,38 +1,15 @@
-#include <string.h>
+#include <string>
 #include "main.h"
 /**
- *
- * @dest:
- * @src:
+ * _strncat -  concatenates two strings.
+ * @dest: first parm
+ * @src: second parm
  * @n:
- * Return: char* 
+ * Return: dest 
  */
+
 char *_strncat(char *dest, char *src, int n)
 {
-int i, j, len, len1, len2;
-char *conc;
-
-i = 0;
-j = 0;
-len1 = strlen(stc);
-len2 = strlen(dest);
-conc = dest;
-while(i < n)
-{
-if (i < len1)
-{
-conc[i] = stc[i];
-}
-else
-{
-while (j < (len2 - 1))
-{
-conc[i] = dest[j];
-j++;
-}
-}
-i++;
-}
-dest =  conc;
+strncat(dest ,src, n);
 return (dest);
 }
