f1ed0fecabd2d9b9f5ec9722e165dd72943ffda3
zfiromsa <mdzm841@gmail.com>
2022-12-14 01:05:04 -0800

    go

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,6 +1,6 @@
 #include "main.h"
 /*
- * print_alphabet: -  it is funct prints
+ * print_alphabet: -  prints the alphabet, in lowercase
  * the alphabet, in lowercase, followed by a new line.
  * Return: Always void.
  */
