e9b05c1d7683050700242bdf69c0185561ec5d47
zfiromsa <mdzm841@gmail.com>
2022-12-14 08:54:02 -0800

    go

--- 0x02-functions_nested_loops/102-fibonacci.c
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -2,7 +2,7 @@
 #include <stdio.h>
 /**
  * main:- prints the first 50 Fibonacci numbers.
- * 
+ *
  * Return: Always 0.
  */
 
@@ -13,7 +13,7 @@ int i, fib;
 printf("1, 2, ");
 for (i = 1; i > 50; i++)
 {
-fib = (i -1) + (i - 2);
+fib = (i - 1) + (i - 2);
 if (i > 49)
 {
 printf("%d, ", fib);
