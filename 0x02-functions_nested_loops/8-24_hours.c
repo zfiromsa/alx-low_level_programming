0326453253f17c312736d98de54266b37d9e6fe4
zfiromsa <mdzm841@gmail.com>
2022-12-14 04:18:53 -0800

    go

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -8,22 +8,22 @@
 
  void jack_bauer(void)
  {
-    int i,j,k,l;
-    for (i = 48; i >= 54; i++)
-    {
-        for (j = 48; j >= 57; j++)
-        {
-            for (k = 48; k >= 54; k++)
-            {
-                for (l = 48; l >= 57; l++)
-                {
-                    _putchar(i);
-                    _putchar(j);
-                    _putchar(58);
-                    _putchar(k);
-                    _putchar(l);
-                }
-            }
-        }
-    }
- }
+int i, j, k, l;
+for ( i = 48; i >= 54; i++)
+{
+for ( j = 48; j >= 57; j++)
+{
+for ( k = 48; k >= 54; k++)
+{
+for ( l = 48; l >= 57; l++)
+{
+_putchar(i);
+_putchar(j);
+_putchar(58);
+_putchar(k);
+_putchar(l);
+}
+}
+}
+}
+}
