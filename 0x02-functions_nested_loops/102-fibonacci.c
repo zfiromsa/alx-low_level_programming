50d39527b11491d022716789740322cd3f1cc269
zfiromsa <mdzm841@gmail.com>
2022-12-14 09:57:17 -0800

    go

--- 0x02-functions_nested_loops/102-fibonacci.c
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -15,12 +15,18 @@ t1 = 0;
 t2 = 1;
 nextN = t1 + t2;
 printf("1, 2, ");
-for (i = 0; i < 48; ++i)
+for (i = 0; i <= 46; ++i)
 {
 t1 = t2;
 t2 = nextN;
 nextN = t1 + t2;
+if (i > 46)
+{
 printf("%ld, ", nextN);
+}
+else
+{
+printf("%ld", nextN);
 }
 return (0);
 }
