f96d13539ea3ba2d99c33e7b65cd7acc26c07726
zfiromsa <mdzm841@gmail.com>
2022-12-15 03:59:44 -0800

    go

---
+++ 0x04-more_functions_nested_loops/100-print_factor.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+/**
+ *
+ *
+ */
+
+int print_factor(void)
+{
+int i, factor;
+
+factor =0;
+for (i = 0; i < 612852475143; i++)
+{
+if ((612852475143 % i) == 0)
+{
+factor = i;
+}
+else
+{
+continue;
+}
+}
+return (0);
+}
