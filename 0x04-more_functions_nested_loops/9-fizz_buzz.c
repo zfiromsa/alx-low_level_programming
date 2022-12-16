0330ce4a530a8ae9d5c84c41a729708c559f4dce
zfiromsa <mdzm841@gmail.com>
2022-12-16 10:56:50 -0800

    go

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 /**
- * Fizz_buzz - it is a program that prints the numbers
+ *  main - it is a program that prints the numbers
  * from 1 to 100, followed by a new line
  *
  * Return: Always 0.
