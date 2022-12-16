fe3e5d6332f95f78ebf3f949dc04fd268630b636
zfiromsa <mdzm841@gmail.com>
2022-12-16 12:51:27 -0800

    go

---
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * print_number: prints an integer.
+ * @n: paramiter.
+ *
+ * Return: Always  void.
+ */
+
+void print_number(int n)
+{
+char m;
+
+m = n + 0;
+_putchar(m);
+}
