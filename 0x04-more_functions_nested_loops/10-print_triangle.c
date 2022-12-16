42e7abca36c68b85f9cfadc9fd1c47a340e6f649
zfiromsa <mdzm841@gmail.com>
2022-12-16 13:37:09 -0800

    go

--- 0x04-more_functions_nested_loops/10-print_triangle.c
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -1,11 +1,14 @@
-#include "main.h" 
-/**
- * print_triangle -prints a triangle, followed \n
- * 
- * @param size
- * Return: void
- */
+ #include <stdio.h>
+void print_triangle(int size);
 
+int main(void)
+{
+    print_triangle(2);
+   // print_triangle(10);
+    //print_triangle(1);
+    //print_triangle(0);
+    return (0);
+}
 void print_triangle(int size)
 {
 int i, j, s, m;
@@ -17,18 +20,22 @@ for (j = 1; j <= size; j++)
 {
 for (i = m; i >= 1; i--)
 {
-_putchar(32);
+printf("kk ii");
+//_putchar(32);
 }
 for (s = 1; s <= size; i++)
 {
-_putchar(35);
+printf("#");
+//_putchar(35);
 }
-_putchar("\n");
+printf(2);
+//_putchar("\n");
 }
 }
 else
 {
-_putchar("\n");
+printf("\n");
+//_putchar("\n");
 }
 }
 
--- 0x04-more_functions_nested_loops/100-prime_factor.c
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -6,21 +6,21 @@
 
 int main(void)
 {
-long long factor, i, G;
-
-G = 612852475143ULL;
+int factor, i, g;
+printf("go");
+g = 613;
 factor =0;
-for (i = 0; i < G; i++)
+for (i = 0; i < g; i++)
 {
-if ((G % i) == 0)
+if ((g % i) == 0)
 {
+printf("go1");
 factor = i;
-}
-else
-{
-continue;
+printf("go2");
 }
 }
+printf("go3");
 printf("%d", factor);
+printf("go4");
 return (0);
 }
