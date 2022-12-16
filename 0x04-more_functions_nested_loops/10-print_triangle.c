aeb9e0a104274267612b961b808dc9afab91c0c0
zfiromsa <mdzm841@gmail.com>
2022-12-16 11:54:49 -0800

    go

--- 0x04-more_functions_nested_loops/10-print_triangle.c
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -13,19 +13,19 @@ int i, j, s;
 s = size - 1;
 if (size > 0)
 {
-for (j = s; j >= 0; j--)
+for (j = 1; j <= s; j++)
 {
 _putchar(32);
-for (i = 1; i < = size; i++)
+for (i = s; i >= 0; i--)
 {
 _putchar(35);
 }
-//_putchar("\n");
+_putchar("\n");
 }
 }
 else
 {
-//_putchar("\n");
+_putchar("\n");
 }
 }
 
