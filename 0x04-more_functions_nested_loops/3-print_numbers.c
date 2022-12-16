6632ce3c62ddc592014e078a8f79a9e608501f99
zfiromsa <mdzm841@gmail.com>
2022-12-16 09:35:53 -0800

    go

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -1,9 +1,8 @@
-#include <stdio.h>
 #include "main.h"
 /**
  * print_numbers - prints the numbers, from 0 to 9,
  * followed by a new line.
- * Return: void
+ * Return: Alwass void.
  */
 
 void print_numbers(void)
