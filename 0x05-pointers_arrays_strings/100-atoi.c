3f6f91bce6e4fbc27c55173b2caf3675a2320bee
zfiromsa <mdzm841@gmail.com>
2022-12-19 09:46:23 -0800

    go

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -14,6 +14,7 @@ int i, len, the_number;
 len = strlen(s);
 for (i = 0; i <= len; i++)
 {
+s[i] = s[i] + 0;
 if ((s[i] >= '48' && s[i] <= '58') || (s[i] == '43') || (s[i] == '45'))
 {
 the_number[i] = s[i] + 0;
