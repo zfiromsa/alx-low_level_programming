17689dbd839c68016de57ee81b2e33ee3cbed2d4
zfiromsa <mdzm841@gmail.com>
2022-12-20 06:39:27 -0800

    go

--- 0x05-pointers_arrays_strings/101-keygen.c
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -1,25 +1,17 @@
+#include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
-#include <stdio.h>
-
 /**
  * main - check the code
  *
  * Return: Always 0.
  */
-int main(void)
-{
-int pass, sum;
 
-srand(time(NULL));
-sum = 0;
-while (sum <= 9999)
+int main ()
 {
-pass = (rand() % 180);
-sum += pass;
-printf("%c", pass);
-}
-printf("%c", 3233 - sum);
-return (0);
-}
+time_t t;
 
+srand((unsigned) time(&t));
+printf("%d\n", rand() % 50);
+return(0);
+}
