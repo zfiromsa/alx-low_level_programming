a95a66768a599766731e1b493f6cb26142c2a641
zfiromsa <mdzm841@gmail.com>
2022-12-19 19:02:59 -0800

    go

---
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -0,0 +1,24 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+int pass, sum;
+
+srand(time(NULL));
+sum = 0;
+while (sum <= 9999)
+{
+pass = (rand() % 180);
+sum += pass;
+printf("%c", pass);
+}
+printf("%c", 3233 - sum)
+return (0);
+}
+
