5f2365bb04f0caba5f68bd7ad21a0addd21420b1
zfiromsa <mdzm841@gmail.com>
2022-12-21 10:55:33 -0800

    go

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -2,14 +2,15 @@
 #include <string.h>
 /**
  * _strncat -  concatenates two strings.
- * @dest: first parm
- * @src: second parm
- * @n: third parm
- * Return: dest
+ * @dest: first parm.
+ * @src: second parm.
+ * @n: third parm.
+ *
+ * Return: dest.
  */
 
 char *_strncpy(char *dest, char *src, int n)
 {
-strncpy(dest, src, n);
+strncpy (dest, src, n);
 return (dest);
 }
