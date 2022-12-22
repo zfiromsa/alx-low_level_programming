c68c47520032ddb3572592a955d7b44af3855bde
zfiromsa <mdzm841@gmail.com>
2022-12-22 11:28:43 -0800

    go

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -12,13 +12,15 @@ char *_strcat(char *dest, char *src)
 int len, i;
 
 len = 0;
-while (dest [len])
+while (dest[len])
+{
 len++;
+}
 for (i = 0; src [i] != '\0'; i++)
 {
-dest [len] = src [i];
+dest[len] = src [i];
 len = len + 1; 
 }
-dest [len] = '\0';
+dest[len] = '\0';
 return (dest);
 }
