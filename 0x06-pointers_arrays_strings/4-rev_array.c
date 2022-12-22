ebbd3c2258b103246e9532590f97486a799d1b9a
zfiromsa <mdzm841@gmail.com>
2022-12-22 00:17:09 -0800

    go

--- 0x06-pointers_arrays_strings/4-rev_array.c
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -1,6 +1,5 @@
 //#include "main.h"
 #include <stdio.h>
-#include <string.h>
 /**
  * reverse_array - reverses the content
  * @a: the string to be reverses
@@ -10,26 +9,15 @@
 
 void reverse_array(int *a, int n)
 {
-int temp;
-    for(int i = 0; i < n/2; i++)
-    {
-        temp = a[i];
-        a[i] = a[n-i-1];
-        a[n-i-1] = temp;
-    }
+int temp, i;
+
+for (i = 0; i < n/2; i++)
+{
+temp = a[i];
+a[i] = a[n-i-1];
+a[n-i-1] = temp;
 }
-
-
-
-
-
-
-
-
-
-
-
-
+}
 
 void print_array(int *a, int n)
 {
