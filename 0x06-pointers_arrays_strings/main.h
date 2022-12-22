44d202a0a611aca8f500431b1be7b408fd5d2d0e
zfiromsa <mdzm841@gmail.com>
2022-12-22 02:20:27 -0800

    go

--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -5,7 +5,7 @@ char *_strcat(char *dest, char *src);
 char *_strncat(char *dest, char *src, int n);
 char *_strncpy(char *dest, char *src, int n);
 void reverse_array(int *a, int n);
-char *string_toupper(char *s);
+char *string_toupper(char *);
 char *leet(char *);
 char *cap_string(char *);
 
