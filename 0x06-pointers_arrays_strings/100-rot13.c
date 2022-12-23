4ad9a9f4702c76531c4af7ae12a0a8a6d231f290
zfiromsa <mdzm841@gmail.com>
2022-12-23 04:47:35 -0800

    go

--- 0x06-pointers_arrays_strings/100-rot13.c
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -1,5 +1,4 @@
 #include "main.h"
-#include <stdio.h>
 /**
  * leet - encodes a string into 1337
  *
@@ -30,32 +29,7 @@ else if(str[i] >= 110 && str[i] <= 122)
 {
 str[i] = str[i] - 13;
 }
-
-else
-{
-}
 i++;
 }
 return (str);
-}
-int main(void)
-{
-    char s[] = "ROT13 (\"rotate by 13 places\", sometimes hyphenated ROT-13) is a simple letter substitution cipher.\n";
-    char *p;
-
-    p = rot13(s);
-    printf("%s", p);
-    printf("------------------------------------\n");
-    printf("%s", s);
-    printf("------------------------------------\n");
-    p = rot13(s);
-    printf("%s", p);
-    printf("------------------------------------\n");
-    printf("%s", s);
-    printf("------------------------------------\n");
-    p = rot13(s);
-    printf("%s", p);
-    printf("------------------------------------\n");
-    printf("%s", s);
-    return (0);
 }
