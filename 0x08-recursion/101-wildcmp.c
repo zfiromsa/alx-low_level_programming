24926eda9d4338bc1ce413e239c63e5793002b77
zfiromsa <mdzm841@gmail.com>
2023-01-05 10:51:53 -0800

    go

--- 0x08-recursion/101-wildcmp.c
+++ 0x08-recursion/101-wildcmp.c
@@ -6,12 +6,13 @@
  * @s1: 
  *  
  */
+int _comp(*s1, *s2, i);
 int wildcmp(char *s1, char *s2)
 {
 int i;
 
 i = 0;
-return (_comp(*s1, *s2, i));
+return (_comp(s1, s2, i));
 }
 
 int _comp(*s1, *s2, i)
