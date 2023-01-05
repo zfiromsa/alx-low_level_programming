5254fb864e5b9711b93c8277c3603daea24046ca
zfiromsa <mdzm841@gmail.com>
2023-01-04 23:49:32 -0800

    go

---
+++ 0x08-recursion/6-is_prime_number.c
@@ -0,0 +1,35 @@
+#include "main.h"
+/**
+ * is_prime_number - prints a string, followed by a new line.
+ *
+ * @n: first parm
+ */
+
+int is_prime_number(int n)
+{
+return (is_prime_number);
+}
+
+/**
+ * @brief 
+ * 
+ * @param i 
+ * @return true 
+ * @return false 
+ */
+int  isPrime(is_prime_number(n), int i = 2)
+{
+if (n <= 2)
+{
+return (0)
+}
+if (n % i == 0)
+{
+return (0);
+}
+if (i * i > n)
+{
+return (1);
+}
+return isPrime(n, i + 1);
+}
