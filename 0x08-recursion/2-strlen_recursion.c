65c60d61ce09c43cb3599f086d1db18961672b3f
zfiromsa <mdzm841@gmail.com>
2023-01-04 22:49:10 -0800

    go

--- 0x08-recursion/2-strlen_recursion.c
+++ 0x08-recursion/2-strlen_recursion.c
@@ -8,13 +8,13 @@
 
 int _strlen_recursion(char *s)
 {
-if (s != '\0')
+if (s == '\0')
 {
-s++;
-return (1 + _strlen_recursion);
+return (0);
 }
 else
 {
-return (0);
+s++;
+return ((1 + _strlen_recursion));
 }
 }
