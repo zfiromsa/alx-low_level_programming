cb2bbc789569634abf350a015eec647b1cb93f18
zfiromsa <mdzm841@gmail.com>
2023-01-10 03:24:56 -0800

    go

--- 0x0A-argc_argv/100-change.c
+++ 0x0A-argc_argv/100-change.c
@@ -1,11 +1,86 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+int change_cents(int value, int i);
 int main(int argc, char* argv[])
 {
-int i;
+int count, value, i;
 
-i = argc - 1;
-printf("%s", argv[i]);
-return (0);
+value = atoi(argv[2]);
+count = argc;
+i = 0;
+if (count != 2)
+{
+printf("Error\n");
+return (1);
+}
+change_cents(value, i);
+return (i);
+}
+
+
+
+
+int change_cents(int value, int i)
+{
+if (value >= 25)
+{
+int tmp;
+tmp = value;
+value -= tmp;
+if (value == 0)
+{
+return (value, i++);
+}
+return (change_cents(value,i), i++);
+}
+
+if (value >= 10)
+{
+int tmp;
+tmp = value;
+value -= tmp;
+if (value == 0)
+{
+return (value, i++);
+}
+return (change_cents(value,i), i++);
+}
+
+if (value >= 5)
+{
+int tmp;
+tmp = value;
+value -= tmp;
+if (value == 0)
+{
+return (value, i++);
+}
+return (change_cents(value,i), i++);
+}
+
+if (value >= 2)
+{
+int tmp;
+tmp = value;
+value -= tmp;
+if (value == 0)
+{
+return (value, i++);
+}
+return (change_cents(value,i), i++);
+}
+
+if (value >= 100)
+{
+int tmp;
+tmp = value;
+value -= tmp;
+if (value == 0)
+{
+return (value, i++);
+}
+return (change_cents(value,i), i++);
+}
+
 }
