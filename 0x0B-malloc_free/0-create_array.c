e27375049b2dd4b572adaa2cfb180c9eddabb717
zfiromsa <mdzm841@gmail.com>
2023-01-10 04:15:18 -0800

    go

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -1,5 +1,6 @@
 #include "main.h"
 #include <stdlib.h>
+#include <stdio.h>
 /**
  * main - function that frees a 2 dimensional grid 
  *        previously created by your alloc_grid function.
@@ -8,7 +9,13 @@
  * @height: parm
  */
 
-void free_grid(int **grid, int height)
+char *create_array(unsigned int size, char c)
 {
-free(alloc_grid);
+char *ar;
+if (size == 0)
+{
+return (NULL);
+}
+ar = malloc(size * sizeof(char));
+return (ar);
 }
