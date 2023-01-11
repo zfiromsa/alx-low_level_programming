61e3d5c613b0795035b08e5818df965be411d923
zfiromsa <mdzm841@gmail.com>
2023-01-10 16:40:13 -0800

    go

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -4,17 +4,19 @@
 /**
  * str_concat - that concatenates two strings.
  *
- * @str: parm
+ * @s1: parm
+ * @s2: parm
  * Return: alaways str_cpy or NULL
  */
 
 char *str_concat(char *s1, char *s2)
 {
 char *str_cnc;
-int i, j, size, s1_len;
+int i, j, k, size, s1_len, s2_len;
 
-size = strlen(s1) + strlen(s2);
+size = strlen(s1) + strlen(s2) + 1;
 s1_len = strlen(s1);
+s2_len = strlen(s2);
 str_cnc = malloc(sizeof(char) * size);
 if (str_cnc == NULL)
 {
@@ -24,9 +26,9 @@ for (i = 0; i < s1_len; i++)
 {
 str_cnc[i] = s1[i];
 }
-for (j = s1_len; j < size; j++)
+for (j = s1_len, k = 0; j < size, k < s2_len; j++, k++)
 {
-str_cnc[s1_len] = str_cnc[size];
+str_cnc[j] = s2[k];
 }
 return (str_cnc);
 }
