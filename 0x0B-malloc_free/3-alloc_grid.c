025cf5978db6efdd676467664406af3a08b1ec0d
zfiromsa <mdzm841@gmail.com>
2023-01-10 22:08:42 -0800

    go

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -14,11 +14,19 @@ int **_return, i, j, l;
 
 _return = malloc(width * sizeof(int *));
 for (l = 0; l < height; l++)
-    _return[l] = malloc(width * sizeof(int));
+{
+_return[l] = malloc(width * sizeof(int));
+}
 if (_return == NULL)
-    return (NULL);
+{
+return (NULL);
+}
 for (i = 0; i < width; i++)
-    for (j = 0; j < height; j++)
-        _return[width][height] = 0;
+{
+for (j = 0; j < height; j++)
+{
+_return[width][height] = 0;
+}
+}
 return(_return);
 }
