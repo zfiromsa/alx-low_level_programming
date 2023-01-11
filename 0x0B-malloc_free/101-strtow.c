cb15fc6e403af21ea315c77fa2a9c5f7ab4896ff
zfiromsa <mdzm841@gmail.com>
2023-01-11 12:09:08 -0800

    go

---
+++ 0x0B-malloc_free/101-strtow.c
@@ -0,0 +1,26 @@
+#include <strlib.h>
+#include <string.h>
+#include "main.h"
+/**
+ * strtow - function that splits a string into words
+ *
+ * @str: input parm
+ * Return: _return
+ */
+
+char **strtow(char *str)
+{
+char **ch;
+int i, len;
+
+len = strlen(str)
+if ((str == NULL) || (len == 0))
+{
+return (NULL);
+}
+ch = malloc(str);
+if (ch == NULL)
+{
+return (NULL);
+}
+}
