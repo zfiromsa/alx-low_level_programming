016d54fc6082e1ba4303ee8d014d34220a4722cb
zfiromsa <mdzm841@gmail.com>
2023-01-12 02:41:02 -0800

    go

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -6,20 +6,21 @@
  * 
  * @nmemb: parm
  * @size: parm
- * Return: Always NULL OR  _return 
+ * Return: Always NULL OR  _return1 
  */
 
 void *_calloc(unsigned int nmemb, unsigned int size)
 {
-int *_return;
+int *_return1;
 
 if (nmemb == 0)
 {
 return (NULL);
 }
-_return = (int *)malloc(sizeof(int) * size);
-if (_return == NULL)
+_return1 = (int *)malloc(sizeof(int) * size);
+if (_return1 == NULL)
 {
 return (NULL);
 }
+return (_return1);
 }
