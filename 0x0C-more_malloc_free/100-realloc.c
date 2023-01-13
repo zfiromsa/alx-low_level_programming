0c78c8c2bc124d3f9e4d91f0fa498ba2a583685a
zfiromsa <mdzm841@gmail.com>
2023-01-13 01:26:42 -0800

    go

---
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -0,0 +1,15 @@
+#include <stdlib.h>
+#include <string.h>
+#include "main.h"
+/**
+ * _realloc -  reallocates a memory block using malloc and free
+ * 
+ * @ptr:
+ * @old_size:
+ * @new_size:
+ * Return: 
+ */
+void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
+{
+
+}
---
+++ 0x0C-more_malloc_free/101-mul.c
@@ -0,0 +1,32 @@
+#include <stdlib.h>
+#include <string.h>
+#include "main.h"
+/**
+ * main - multiplies two positive numbers.
+ *
+ * argv: parm
+ * argc: parm
+ * Return: Always 0.
+ * Exit: 98.
+ */
+
+int main(int argc, char *argv[])
+{
+int mul, num1, num2;
+
+if (argc < 3)
+{
+printf("Error\n");
+exit(98);
+}
+num1 = atoi(argv[1]);
+num2 = atoi(argv[2]);
+if (isdigit(num1) || isdigit(num2))
+{
+mul = num1 * num2;
+printf("%d\n", mul);
+return (0);
+}
+printf("Error\n");
+exit (98);
+}
