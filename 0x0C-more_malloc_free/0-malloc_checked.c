a0d52a03349261b151749ac5848b2dea225004f7
zfiromsa <mdzm841@gmail.com>
2023-01-12 22:30:14 -0800

    go

--- 0x0C-more_malloc_free/0-malloc_checked.c
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -5,20 +5,21 @@
  * malloc_checked - that allocates memory using malloc.
  *
  * @b: parm
+ * Return: always p.
  */
- 
+
 void *malloc_checked(unsigned int b)
 {
 int *p;
 
-if (b == 0)
+if (b <= 0)
 {
-return (NULL);
+exit (98);
 }
 p = (int *)malloc(sizeof(char) * b);
 if (p == NULL)
 {
-return (NULL);
+exit (98);
 }
 return (p);
 }
