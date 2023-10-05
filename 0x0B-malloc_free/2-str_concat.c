e74ce1e64c76f693488fe2e2a0158fc351d38793
zfiromsa <mdzm841@gmail.com>
2023-10-05 22:38:24 +0300

    go

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -4,33 +4,72 @@
 #include <stdbool.h>
 #include <string.h>
 
+/**
+ * str_concat - a function that concatenates two strings.
+ *
+ * @s1: first parm
+ * @s2: second parm
+ * Return: NULL or conc
+ */
 char *str_concat(char *s1, char *s2)
 {
-char *conc;
-unsigned int _size, i, j;
+	char *conc;
+	unsigned int _size, i, j, _s1, _s2;
 
-i = 0;
-j = 0;
-_size = strlen(s1) + strlen(s2);
-conc = (char *)malloc(sizeof(char) * _size);
-if (conc == NULL)
-{
-return (NULL);
+	i = 0;
+	j = 0;
+	if (s1 == NULL  && s2 == NULL)
+	{
+		_s1 = 0;
+		_s2 = 0;
+	}
+	else if (s1 == NULL)
+	{
+		_s1 = 0;
+	}
+	else if (s2 == NULL)
+	{
+		_s2 = 0;
+	}
+	else if (s1 != NULL  && s2 != NULL)
+	{
+		_s1 = strlen(s1);
+		_s2 = strlen(s2);
+	}
+	else if (s1 == NULL)
+	{
+		_s1 = strlen(s1);
+	}
+	else if (s2 == NULL)
+	{
+		_s2 = strlen(s2);
+	}
+	_size = _s1 + _s2;
+	conc = (char *)malloc(sizeof(char) * _size);
+	if (conc == NULL)
+	{
+		return (NULL);
+	}
+	while (s1 != NULL)
+	{
+		if (s1[i] == '\0')
+		{
+			break;
+		}
+		conc[i] = s1[i];
+		i++;
+	}
+	if (s2 == NULL)
+	{
+		conc[i] = '\0';
+		return (conc);
+	}
+	while (_size >= i || s2 != NULL)
+	{
+		conc[i] = s2[j];
+		j++;
+		i++;
+	}
+	return (conc);
 }
-while (true)
-{
-if (s1[i] == '\0')
-{
-break;
-}
-conc[i] = s1[i];
-i++;
-}
-while (_size >= i)
-{
-conc[i] = s2[j];
-j++;
-i++;
-}
-return (conc);
-}
+
