a33296e0cb1344799d98c1029995c4366afaeca0
zfiromsa <mdzm841@gmail.com>
2023-10-09 23:11:33 +0300

    go

---
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -0,0 +1,20 @@
+#include "dog.h"
+
+/**
+ * init_dog - a function that initialize a variable of type struct dog.
+ *
+ * @d: struct
+ * @name: First member
+ * @age: Second member
+ * @owner: Third member
+*/
+dog_t *new_dog(char *name, float age, char *owner)
+{
+    struct dog *dog_t;
+    
+	dog_t->name = name;
+	dog_t->age = age;
+	dog_t->owner = owner;
+    return (dog_t);
+}
+
---
+++ 0x0E-structures_typedef/5-free_dog.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "dog.h"
+/**
+ * free_dog - it free alocated pointer
+ *
+ * Return: nothing
+ */
+
+void free_dog(dog_t *d)
+{
+	free(d->name);
+	(void)d->age;
+	free(d->owner);
+	free(d);
+}
+
