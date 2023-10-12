cd5c8221dfab5dc092100b9a62dbb6b06c87ec41
zfiromsa <mdzm841@gmail.com>
2023-10-12 08:37:11 +0300

    0x0F-function_pointers

--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -12,7 +12,7 @@ void array_iterator(int *array, size_t size, void (*action)(int))
 	size_t i;
 
 	i = 0;
-	if (action == NULL || size <= 0)
+	if (action == NULL || size <= 0 || array == NULL)
 		exit(0);
 	while (i < size)
 	{
--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -13,8 +13,8 @@ int int_index(int *array, int size, int (*cmp)(int))
 	int i;
 
 	i = 0;
-	if (cmp == NULL)
-		return (1);
+	if (cmp == NULL || array == NULL || size <= 0)
+		return (-1);
 	if (size <= 0)
 		return (-1);
 	while (i < size)
