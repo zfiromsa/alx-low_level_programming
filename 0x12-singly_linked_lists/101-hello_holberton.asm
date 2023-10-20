67de249bfc398c36e6d83e84e8dd337cb939f71f
zfiromsa <mdzm841@gmail.com>
2023-10-20 23:47:59 +0300

    go

--- 0x12-singly_linked_lists/101-hello_holberton.asm
+++ 0x12-singly_linked_lists/101-hello_holberton.asm
@@ -1,16 +1,17 @@
 section .data
-    msg db "Hello, Holberton",10      ; 10 is the ASCII code for a new line (LF)
+	msg db "Hello, Holberton",10	; 10 is the ASCII code for a new line (LF)
 
 section .text
-    global _start
+	global _start
 
 _start:
-    mov rax, 1
-    mov rdi, 1
-    mov rsi, msg
-    mov rdx, 13
-    syscall
-    
-    mov rax, 60
-    mov rdi, 0
-    syscall
+	mov rax, 1
+	mov rdi, 1
+	mov rsi, msg
+	mov rdx, 13
+	syscall
+
+	mov rax, 60
+	mov rdi, 0
+	syscall
+
