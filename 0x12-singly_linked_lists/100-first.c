a7d5fbee4bc9d8bdd5d7db8d6a48bdcd0d39d880
zfiromsa <mdzm841@gmail.com>
2023-10-20 23:40:54 +0300

    go

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -6,7 +6,6 @@ void beforeMain (void) __attribute__((constructor));
 */
 void beforeMain (void)
 {
-	printf ("You're beat! and yet, you must allow,
-			\nI bore my house upon my back!\n");
+	printf ("You're beat! and yet, you must allow,\nI bore my house upon my back!\n");
 }
 
