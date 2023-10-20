ab8bcd2a7fa89fe8adfe78989be4c87bde7a75cb
zfiromsa <mdzm841@gmail.com>
2023-10-20 23:39:36 +0300

    go

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,12 @@
+#include "lists.h"
+
+void beforeMain (void) __attribute__((constructor));
+/**
+ * beforeMain - this function printed before main.
+*/
+void beforeMain (void)
+{
+	printf ("You're beat! and yet, you must allow,
+			\nI bore my house upon my back!\n");
+}
+
