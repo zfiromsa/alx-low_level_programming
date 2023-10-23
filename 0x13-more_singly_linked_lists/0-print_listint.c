b3da721ff02111be67cc152a168b4ce10c8a6cb6
zfiromsa <mdzm841@gmail.com>
2023-10-23 10:02:03 +0300

    go

---
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -0,0 +1,20 @@
+#include "lists.h"
+
+/**
+ * print_listint - prints all the elements of a listint_t list.
+ * @h: it is a pointer to the listint_t list.
+ * Return: count
+ */
+size_t print_listint(const listint_t *h)
+{
+	size_t count;
+
+	count = 0;
+	while (h != NULL)
+	{
+		printf("%d\n", h->n);
+		count++;
+	}
+	return (count);
+}
+
---
+++ 0x13-more_singly_linked_lists/README.md
@@ -0,0 +1,3 @@
+##0x13. C - More singly linked lists
+
+A singly linked list is a linear data structure in which the elements are not stored in contiguous memory locations and each element is connected only to its next element using a pointer.
