50042261c4d80e00ddbd29bba8fd2fd89baaa5c6
zfiromsa <mdzm841@gmail.com>
2023-10-24 13:41:46 +0300

    go

---
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -0,0 +1,28 @@
+#include "lists.h"
+
+int delete_nodeint_at_index(listint_t **head, unsigned int index)
+{
+	listint_t *tmp1, *tmp2;
+    unsigned int _index;
+
+    _index = index - 1;
+	tmp1 = (listint_t *)malloc(sizeof(listint_t));
+	if (tmp1 == NULL)
+		return (-1);
+	tmp2 = (listint_t *)malloc(sizeof(listint_t));
+	if (tmp2 == NULL)
+		return (-1);
+	while (_index--)
+	{
+		if ((*head)->next == NULL)
+			return (-1);
+		(*head) = (*head)->next;
+	}
+    tmp1 = (*head)->next;
+    (*head) = (*head)->next;
+    tmp2 = (*head)->next;
+    free(head);
+    tmp1->next = tmp2;
+    return (1);
+}
+
