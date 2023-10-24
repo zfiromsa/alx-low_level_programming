c225a4b7baef63379687367e17fbbd24f0d1d1ad
zfiromsa <mdzm841@gmail.com>
2023-10-24 09:49:09 +0300

    go

--- 0x13-more_singly_linked_lists/4-free_listint.c
+++ 0x13-more_singly_linked_lists/4-free_listint.c
@@ -2,16 +2,9 @@
 
 void free_listint(listint_t *head)
 {
-listint_t *new;
+	if (head == NULL)
+		exit (98);
+	free_listint(head->next);
+	free(head);
+}
 
-new = (listint_t *)malloc(sizeof(listint_t));
-if (new == NULL || head == NULL)
-{
-exit (98);
-}
-new = head;
-head = head->next;
-free(new->next);
-free(new);
-free_listint(head);
-}
