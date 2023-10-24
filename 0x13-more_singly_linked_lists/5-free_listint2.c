e27c771f36b69c11101b31f4860f30ebedaa8cca
zfiromsa <mdzm841@gmail.com>
2023-10-24 10:19:06 +0300

    go

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -10,8 +10,8 @@ void free_listint2(listint_t **head)
 	listint_t *tmp;
 	while (head != NULL)
 	{
-		tmp = head;
-		head = (*head)->next;
+		tmp = (*head);
+		(*head) = (*head)->next;
 		free(tmp);
 	}
 }
