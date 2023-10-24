f875d531347e1f649e1f0abb9dbb168308e2b7bc
zfiromsa <mdzm841@gmail.com>
2023-10-24 10:06:31 +0300

    go

--- 0x13-more_singly_linked_lists/4-free_listint.c
+++ 0x13-more_singly_linked_lists/4-free_listint.c
@@ -1,10 +1,18 @@
 #include "lists.h"
 
+/**
+ * free_listint - free a lists_t.
+ *
+ * @head: it point to lists_t lists.
+ */
 void free_listint(listint_t *head)
 {
-	if (head == NULL)
-		exit (98);
-	free_listint(head->next);
-	free(head);
+	listint_t *tmp;
+	while (head != NULL)
+	{
+		tmp = head;
+		head = head->next;
+		free(tmp);
+	}
 }
 
