e5b5596f6d3e0fc6dfc6aca0ce24a00f18b01170
zfiromsa <mdzm841@gmail.com>
2023-10-24 10:10:46 +0300

    go

---
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,18 @@
+#include "lists.h"
+
+/**
+ * free_listint2 - free a lists_t.
+ *
+ * @head: it point to lists_t lists.
+ */
+void free_listint2(listint_t **head)
+{
+	listint_t *tmp;
+	while (head != NULL)
+	{
+		tmp = head;
+		head = (*head)->next;
+		free(tmp);
+	}
+}
+
