b07cd9f5c85fb0e294cdca24d6fbd006130198a6
zfiromsa <mdzm841@gmail.com>
2023-10-24 13:59:30 +0300

    go

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -19,7 +19,7 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 	{
 		if ((*head)->next == NULL)
 		{
-			fre(tmp);
+			free(tmp);
 			return (NULL);
 		}
 		(*head) = (*head)->next;
