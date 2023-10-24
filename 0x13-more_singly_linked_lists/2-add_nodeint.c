498b06b493932d5aa5c2f5809ebca9237f066259
zfiromsa <mdzm841@gmail.com>
2023-10-24 09:14:07 +0300

    go

--- 0x13-more_singly_linked_lists/2-add_nodeint.c
+++ 0x13-more_singly_linked_lists/2-add_nodeint.c
@@ -2,8 +2,9 @@
 
 /**
  * add_nodeint - unction that adds a new node at the beginning of a list
- * 
- * 
+ * @head: it is the address of head list.
+ * @n: the value new list.
+ * Return: head or NULL
 */
 listint_t *add_nodeint(listint_t **head, const int n)
 {
