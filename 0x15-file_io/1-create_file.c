931c92f5e8a4ced90115208bbee9c2b3f8dc10a2
zfiromsa <mdzm841@gmail.com>
2023-10-31 01:15:21 +0300

    go

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -1,28 +1,27 @@
-#include <fcntl.h>
 #include "main.h"
-
+/**
+ * create_file - Create a function that creates a file.
+ *
+ * @filename: it is parm
+ * @text_countent:  it is parm
+ * Return: 1 or -1
+ */
 int create_file(const char *filename, char *text_content)
 {
-    int fd;
-    ssize_t leng;
-    mode_t mode;
-    
-    mode = S_IRUSR | S_IWUSR;
-    leng = strlen(text_content);
-    if (filename == NULL)
-    {
-    return (-1);
-    }
-    fd = open(filename, O_WRONLY | O_CREAT | O_TRUNC, mode);
-    if (fd == -1)
-    {
-        return (-1);
-    }
-    if (text_content != NULL)
-    {
-        write(fd, text_content, leng);
-    }
-    close(fd);
-    return (1);
+	int fd;
+	ssize_t leng;
+	mode_t mode;
+
+	mode = S_IRUSR | S_IWUSR;
+	leng = strlen(text_content);
+	if (filename == NULL)
+		return (-1);
+	fd = open(filename, O_WRONLY | O_CREAT | O_TRUNC, mode);
+	if (fd == -1)
+		return (-1);
+	if (text_content != NULL)
+		write(fd, text_content, leng);
+	close(fd);
+	return (1);
 }
 
---
+++ 0x15-file_io/2-append_text_to_file.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+
+int append_text_to_file(const char *filename, char *text_content)
+{
+int fd;
+ssize_t leng;
+
+leng = strlen(text_content);
+fd = open(filename, O_EXCL | O_APPEND);
+if (fd == -1)
+return (-1);
+write(fd, text_content, leng);
+close(fd);
+}
