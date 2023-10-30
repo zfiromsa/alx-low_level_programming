956cab74e46fdd0a3631394cd1765a298185ffeb
zfiromsa <mdzm841@gmail.com>
2023-10-31 01:33:23 +0300

    go

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -3,7 +3,7 @@
  * create_file - Create a function that creates a file.
  *
  * @filename: it is parm
- * @text_countent:  it is parm
+ * @text_content:  it is parm
  * Return: 1 or -1
  */
 int create_file(const char *filename, char *text_content)
--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -1,5 +1,12 @@
 #include "main.h"
 
+/**
+ * append_text_to_file - that appends text at the end of a file.
+ *
+ * @filename: it is parm
+ * @text_countent:  it is parm
+ * Return: 1 or -1
+ */
 int append_text_to_file(const char *filename, char *text_content)
 {
 	int fd;
@@ -11,6 +18,6 @@ int append_text_to_file(const char *filename, char *text_content)
 		return (-1);
 	write(fd, text_content, leng);
 	close(fd);
-	return (-1);
+	return (1);
 }
 
