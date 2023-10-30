b7dec5a1552913e2c0f456da967c804aea4111f5
zfiromsa <mdzm841@gmail.com>
2023-10-31 01:17:15 +0300

    go

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -1,15 +1,16 @@
 #include "main.h"
 
-
 int append_text_to_file(const char *filename, char *text_content)
 {
-int fd;
-ssize_t leng;
+	int fd;
+	ssize_t leng;
 
-leng = strlen(text_content);
-fd = open(filename, O_EXCL | O_APPEND);
-if (fd == -1)
-return (-1);
-write(fd, text_content, leng);
-close(fd);
+	leng = strlen(text_content);
+	fd = open(filename, O_EXCL | O_APPEND);
+	if (fd == -1)
+		return (-1);
+	write(fd, text_content, leng);
+	close(fd);
+	return (-1);
 }
+
