8e0501de7c3a130a5255d522263ae775d580e493
zfiromsa <mdzm841@gmail.com>
2023-10-31 16:11:17 +0300

    go

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -12,12 +12,14 @@ int append_text_to_file(const char *filename, char *text_content)
 	int fd;
 	ssize_t leng, ret;
 
+	leng = 0;
 	if (filename == NULL)
 		return (-1);
 	fd = open(filename, O_WRONLY | O_APPEND);
 	if (fd == -1)
 		return (-1);
-	leng = strlen(text_content);
+	if (text_content != NULL)
+		leng = strlen(text_content);
 	ret = write(fd, text_content, leng);
 	if (ret != leng)
 	{
