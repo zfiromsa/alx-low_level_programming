dbd78d1b9c678beb45c0120eff294cb1c51a86da
zfiromsa <mdzm841@gmail.com>
2023-10-31 09:12:00 +0300

    go

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -1,10 +1,10 @@
 #include "main.h"
 
 /**
- * append_text_to_file - that appends text at the end of a file.
+ * append_text_to_file - appends text at the end of a file.
  *
  * @filename: it is parm
- * @text_countent:  it is parm
+ * @text_content:  it is parm
  * Return: 1 or -1
  */
 int append_text_to_file(const char *filename, char *text_content)
@@ -15,7 +15,7 @@ int append_text_to_file(const char *filename, char *text_content)
 	fd = open(filename, O_EXCL | O_APPEND);
 	if (fd == -1)
 		return (-1);
-	leng = strlen(text_content) + sizeof(fd);
+	leng = strlen(text_content);
 	write(fd, text_content, leng);
 	close(fd);
 	return (1);
