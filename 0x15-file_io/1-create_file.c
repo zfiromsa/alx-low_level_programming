55c78a71405b42a8f5bfbc4bb72281a6133cb2cb
zfiromsa <mdzm841@gmail.com>
2023-10-31 08:30:05 +0300

    go

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -14,6 +14,8 @@ int create_file(const char *filename, char *text_content)
 
 	mode = S_IRUSR | S_IWUSR;
 	leng = strlen(text_content);
+	if (text_content == NULL)
+		return (1);
 	if (filename == NULL)
 		return (-1);
 	fd = open(filename, O_WRONLY | O_CREAT | O_TRUNC, mode);
