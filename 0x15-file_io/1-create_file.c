da2ef0bd197aebbb693296032abcd59ccde748b5
zfiromsa <mdzm841@gmail.com>
2023-10-31 08:34:11 +0300

    go

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -14,13 +14,16 @@ int create_file(const char *filename, char *text_content)
 
 	mode = S_IRUSR | S_IWUSR;
 	leng = strlen(text_content);
-	if (text_content == NULL)
-		return (1);
 	if (filename == NULL)
 		return (-1);
 	fd = open(filename, O_WRONLY | O_CREAT | O_TRUNC, mode);
 	if (fd == -1)
 		return (-1);
+	if (text_content ==NULL)
+	{
+		close(fd);
+		return (1);
+	}
 	if (text_content != NULL)
 		write(fd, text_content, leng);
 	close(fd);
