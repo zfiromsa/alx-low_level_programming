7edfbf0c26841af8784d0c9c063317deb4905f77
zfiromsa <mdzm841@gmail.com>
2023-11-01 09:53:36 +0300

    go

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -13,7 +13,9 @@ int create_file(const char *filename, char *text_content)
 	mode_t mode;
 
 	mode = S_IRUSR | S_IWUSR;
-	leng = strlen(text_content);
+	leng = 0;
+	if (text_content != NULL)
+		leng = strlen(text_content);	
 	if (filename == NULL)
 		return (-1);
 	fd = open(filename, O_WRONLY | O_CREAT | O_TRUNC, mode);
--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -2,26 +2,8 @@
 
 int main(int argc, char **argv)
 {
-int fd_from, fd_to;
-ssize_t leng_from;
 
-if (argc != 3)
-{
-    exit(97);
-}
-leng_from = strlen(argv[2]);
-fd_from = open(argv[2], O_RDONLY);
-if (fd_from == -1)
-{
-    exit(98);
-}
 fd_to = open(argv[2], O_RDWR | O_CREAT | O_EXCL, S_IRUSR | S_IWUSR);
-if (fd_to == -1)
-{
-    exit(99);
-}
-write(fd_to, fd_from, leng_from);
-close(fd_from);
-return (1);
+
 }
 
