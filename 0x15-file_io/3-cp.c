3f1f6398a813933d5b8ac357b428f517214385b8
zfiromsa <mdzm841@gmail.com>
2023-11-03 12:46:01 +0300

    go

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -2,49 +2,56 @@
 
 int main(int argc, char **argv)
 {
-int fd[2], i;
-char buff[Zbuff];
-mode_t mode;
-ssize_t _read, _written;
+	char buffer[BUFFER];
+	int fd_to, fd_from;
+	ssize_t _read, _write;
+	mode_t mode;
 
-mode = S_IRUSR | S_IWUSR | S_IRGRP | S_IWGRP | S_IROTH;
-i = 0;
-if (argc != 3)
-{
-dprintf(STDERR_FILENO, "Usage: cp file_from file_to");
-exit(97);
-}
-fd[0] = open(argv[0], O_RDONLY);
-if (fd[0] == -1)
-{
-      dprintf(STDERR_FILENO, "Error: Can't read from file NAME_OF_THE_FILE %s", argv[0]);
-exit(98);
-}
-fd[1] = open(argv[1], O_WRONLY | O_CREAT | O_TRUNC, mode);
-if (fd[1] == -1)
-{
-dprintf(STDERR_FILENO, "Error: Can't write to NAME_OF_THE_FILE %s", argv[1]);
-i = close(fd[0]);
-if (i != 0)
-{
-dprintf(2, "Error: Can't close fd FD_VALUE");
-exit(100);
-}
-exit(99);
-}
-while ((_read = read(fd[0], buff, Zbuff)) > 0)
-  {
-    _written = write(fd[1], buff, _read);
-    if (_written != _read)
-    {
-      dprintf(STDERR_FILENO, "Error: Can't write to NAME_OF_THE_FILE %s", argv[1]);
-      close(fd[0]);
-      close(fd[1]);
-      exit(99);
-    }
-  }
-close(fd[0]);
-close(fd[1]);
-return (0);
+	mode = S_IRUSR | S_IWUSR | S_IRGRP | S_IWGRP | S_IROTH;
+	if (argc != 3)
+	{
+		dprintf(STDOUT_FILENO, USAGE);
+		exit(97);
+	}
+	fd_from = open(argv[0], O_RDONLY);
+	if (fd_from == -1)
+	{
+		dprintf(STDOUT_FILENO, CANT_READ, argv[0]);
+		exit(98);
+	}
+	fd_to = open(argv[1], O_WRONLY | O_CREAT | O_TRUNC, mode);
+	if (fd_to == -1)
+	{
+		dprintf(STDOUT_FILENO, CANT_WRITE, argv[1]);
+		if (close(fd_from) != 0)
+		{
+			dprintf(STDOUT_FILENO, CANT_CLOSE);
+			exit(100);
+		}
+		exit(98);
+	}
+	_read = read(fd_from, buffer, BUFFER);
+	if (_read < 0)
+	{
+		dprintf(STDOUT_FILENO, CANT_READ, argv[0]);
+		exit(98);
+	}
+	_write = write(fd_to, buffer, BUFFER);
+	if (_write < 0)
+	{
+		dprintf(STDOUT_FILENO, CANT_WRITE, argv[0]);
+		exit(98);
+	}
+	if (close(fd_to) != 0)
+	{
+		dprintf(STDOUT_FILENO, CANT_CLOSE);
+		exit(100);
+	}
+	if (close(fd_from) != 0)
+	{
+		dprintf(STDOUT_FILENO, CANT_CLOSE);
+		exit(100);
+	}
+	return (0);
 }
 
--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -3,7 +3,11 @@
 
 #define _POSIX_C_SOURCE  200809L
 #define _GNU_SOURCE
-#define Zbuff 1024
+#define BUFFER 1024
+#define USAGE "Usage: cp file_from file_to"
+#define CANT_READ "Error: Can't read from file %s"
+#define CANT_WRITE "Error: Can't write to %s"
+#define CANT_CLOSE "Error: Can't close fd FD_VALUE"
 
 #include <stdio.h>
 #include <stdlib.h>
