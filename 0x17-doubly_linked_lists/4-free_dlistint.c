84d2c8eef00fba0d1f050f0f6e1cefdc0234e992
zfiromsa <mdzm841@gmail.com>
2023-12-08 03:50:05 -0800

    go

--- 0x17-doubly_linked_lists/4-free_dlistint.c
+++ 0x17-doubly_linked_lists/4-free_dlistint.c
@@ -1,5 +1,7 @@
 #include "lists.h"
-
+/**
+ * free_dlistint -  frees a dlistint_t list.
+*/
 void free_dlistint(dlistint_t *head)
 {
 dlistint_t *tmp;
--- 0x17-doubly_linked_lists/5-get_dnodeint.c
+++ 0x17-doubly_linked_lists/5-get_dnodeint.c
@@ -1,7 +1,22 @@
 #include "lists.h"
-
+/**
+ * get_dnodeint_at_index - func that returns the nth node of a dlistint_t linked list.
+ *
+ * @head: head of list.
+ * @index: index.
+ * Return: NULL of head.
+*/
 dlistint_t *get_dnodeint_at_index(dlistint_t *head, unsigned int index)
 {
+	int count;
 
-
+	count = 0;
+	while (head)
+	{
+	if (count == index)
+		return (head);
+	head = head->next;
+	}
+	return (NULL);
 }
+
