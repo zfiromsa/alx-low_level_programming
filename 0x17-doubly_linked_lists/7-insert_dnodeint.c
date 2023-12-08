cf0541e32b6733fc447f617fa6adae426ec9e6d9
zfiromsa <mdzm841@gmail.com>
2023-12-08 04:23:32 -0800

    go

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -2,6 +2,28 @@
 
 dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 {
+	unsigned int count;
+    dlistint_t *tmp, *_newlist;
 
+
+	_newlist = malloc(sizeof(dlistint_t));
+	if (_newlist == NULL)
+	{
+		return (NULL);
+	}
+	count = 0;
+	while (*h)
+	{
+	if (count == idx)
+	{
+        tmp = *h;
+        _newlist = add_dnodeint((*h)->next, n);
+        _newlist = add_dnodeint_end(_newlist, tmp);
+        return (_newlist);
+    }
+	(*h) = (*h)->next;
+	count++;
+	}
+	return (NULL);
 }
 
