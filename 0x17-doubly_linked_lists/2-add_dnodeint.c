822bfb9a94d684e6fc68c501f8436f35b5a15e63
zfiromsa <mdzm841@gmail.com>
2023-12-08 02:34:48 -0800

    go

--- 0x17-doubly_linked_lists/2-add_dnodeint.c
+++ 0x17-doubly_linked_lists/2-add_dnodeint.c
@@ -3,6 +3,7 @@
  * add_dnodeint - hat adds a new node at the beginning of a dlistint_t list.
  *
  * @head: head of list.
+ * @n: number
  * Return: NULL OR _newlist.
 */
 dlistint_t *add_dnodeint(dlistint_t **head, const int n)
--- 0x17-doubly_linked_lists/3-add_dnodeint_end.c
+++ 0x17-doubly_linked_lists/3-add_dnodeint_end.c
@@ -1,7 +1,27 @@
 #include "lists.h"
 
+/**
+ * add_dnodeint_end - adds a new node at the end of a dlistint_t list.
+ *
+ * @head: head of list.
+ * @n: number
+ * Return: NULL OR _newlist.
+*/
 dlistint_t *add_dnodeint_end(dlistint_t **head, const int n)
 {
+	dlistint_t *_newlist;
 
+	_newlist = malloc(sizeof(dlistint_t));
+	if (_newlist == NULL)
+	{
+		return (NULL);
+	}
+	_newlist->next = NULL;
+	_newlist->n = n;
+	while (head)
+		head = (*head)->next;
+	_newlist->prev = *head;
+
+	return (head);
 }
 
