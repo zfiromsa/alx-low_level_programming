fda24773d0ff985b71fface3f6c030e68d5fa27f
zfiromsa <mdzm841@gmail.com>
2023-12-27 06:43:55 -0800

    go

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -13,7 +13,7 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 	hash_node_t *node;
 
 	i = 0;
-	index = key_index(key, ht->size);
+	index = key_index((unsigned)key, ht->size);
 	node = (hash_node_t *)malloc(sizeof(hash_node_t));
 	if (!node)
 		return (0);
