3f42b7b2dc7b8775e0f12c920658051a7e93350a
zfiromsa <mdzm841@gmail.com>
2023-12-27 18:54:13 -0800

    go

--- 0x1A-hash_tables/4-hash_table_get.c
+++ 0x1A-hash_tables/4-hash_table_get.c
@@ -10,7 +10,7 @@
 char *hash_table_get(const hash_table_t *ht, const char *key)
 {
 	unsigned long int index;
-	if (!ht || !key || strlen(key))
+	if (!ht || !key || !strlen(key))
 		return (NULL);
 	index = key_index((const unsigned char *)key, ht->size);
 	if(!ht->array[index])
