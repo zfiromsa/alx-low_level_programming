b2b455a1e89778248526e0b2681fd73b2f4d2d93
zfiromsa <mdzm841@gmail.com>
2023-12-27 18:45:24 -0800

    go

---
+++ 0x1A-hash_tables/4-hash_table_get.c
@@ -0,0 +1,20 @@
+#include "hash_tables.h"
+
+/**
+ * hash_table_get - a function that retrieves a value associated with a key.
+ *
+ * @ht: is the hash table you want to look into.
+ * @key: is the key you are looking for.
+ * Return: NULL or value
+*/
+char *hash_table_get(const hash_table_t *ht, const char *key)
+{
+	unsigned long int index;
+	if (!ht || !key || strlen(key))
+		return (NULL);
+	index = key_index(key, ht->size);
+	if(!ht->array[index])
+		return (NULL);
+	return (ht->array[index]->value);
+}
+
