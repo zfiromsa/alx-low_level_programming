6cfb5e98062d3af94516fed68c7d5857de6e7275
zfiromsa <mdzm841@gmail.com>
2023-12-27 18:30:44 -0800

    go

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -14,7 +14,7 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 
 	if (!key || strlen(key) == 0 || !ht)
 		return (0);
-	index = key_index((const unsigned char*)key, ht->size);
+	index = key_index((const unsigned char *)key, ht->size);
 	tmp = ht->array[index];
 	while (tmp)
 	{
